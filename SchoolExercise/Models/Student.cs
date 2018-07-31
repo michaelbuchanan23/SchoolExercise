1af60170256cd1c70cb19765b7b22ac87e65f669
michaelbuchanan23 <michaelbuchanan23@gmail.com>
2018-07-31 08:05:03 -0400

    backup 7/31/2018

--- SchoolExercise/Models/Student.cs
+++ SchoolExercise/Models/Student.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -7,7 +8,11 @@ namespace SchoolExercise.Models {
 	public class Student {
 
 		public int Id { get; set; }
+
+		[Display(Name = "First Name")]
 		public string Firstname { get; set; }
+
+		[Display(Name = "Last Name")]
 		public string Lastname { get; set; }
 		public int MajorId { get; set; }
 		public virtual Major Major { get; set;}
